8408df40a4abb87bca1353801b441c15d789da99
Will Moretz <whmoretz@gmail.com>
2023-05-30 16:50:06 -0400

    set up default route

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <>
+      <header>Header</header>
+      <Routes>
+        <Route path="/" element={<section>Homepage</section>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
