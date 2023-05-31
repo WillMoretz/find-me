fdb0ce75f37f83146a918a11e0b2532a3af2965d
Will Moretz <whmoretz@gmail.com>
2023-05-30 23:26:46 -0400

    created pages file to store path names and img sources

---
+++ src/pages.js
@@ -0,0 +1,11 @@
+import goldRush from "./img/gold-rush.jpg";
+import hedgeMaze from "./img/hedge-maze.jpg";
+
+export const GOLD_RUSH = {
+  path: "/gold-rush",
+  src: goldRush,
+};
+export const HEDGE_MAZE = {
+  path: "/hedge-maze",
+  src: hedgeMaze,
+};
