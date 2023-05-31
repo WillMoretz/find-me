7ef1dc9ac2c41da1bd6fd0f7a739a06bad02eeef
Will Moretz <whmoretz@gmail.com>
2023-05-30 22:52:32 -0400

    added link to gold rush page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import goldRush from "../img/gold-rush.jpg";
+import "../styles/home.scoped.css";
 
 function Home() {
-  return <section>Homepage</section>;
+  return (
+    <section>
+      <div>
+        <img src={goldRush} alt="the gold rush where's waldo" />
+        <Link to="/gold-rush">Play</Link>
+        <button type="button">Show Leaderboard</button>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
