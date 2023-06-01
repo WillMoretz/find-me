50cd9837bd24902e67f7e6ab40b0021f481e93a6
Will Moretz <whmoretz@gmail.com>
2023-05-31 21:10:38 -0400

    created info component

---
+++ src/Components/Info.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="info-container">
+      <div className="top-bar">
+        <h2>Info</h2>
+        <button type="button">Close</button>
+      </div>
+      <p>explanation</p>
+      <div>image of waldo</div>
+    </div>
+  );
+}
+
+export default Info;
