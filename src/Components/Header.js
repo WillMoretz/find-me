e4dc6377fcc2774ac6d0154992d0f8957c8411bf
Will Moretz <whmoretz@gmail.com>
2023-05-31 21:26:12 -0400

    imported and added info and overlay components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Info from "./Info";
+import Overlay from "./Overlay";
 import "../styles/header.scoped.css";
 
 function Header() {
+  const [shouldDisplayInfo, setShouldDisplayInfo] = useState(false);
+
   return (
-    <header>
-      <h1>Where's Waldo Online</h1>
-      <Link to="/leaderboard">Leaderboard</Link>
-      <button type="button">Info</button>
-    </header>
+    <>
+      <header>
+        <h1>Where's Waldo Online</h1>
+        <Link to="/leaderboard">Leaderboard</Link>
+        <button type="button">Info</button>
+      </header>
+      <>
+        <Info />
+        <Overlay />
+      </>
+    </>
   );
 }
 
