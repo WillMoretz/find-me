285a20b959a50e0122f7be393ad56ac8b2aeef7d
Will Moretz <whmoretz@gmail.com>
2023-06-01 23:42:45 -0400

    added mouse drag scrolling

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -9,14 +9,44 @@ function Game() {
 
   // Scroll Image by Dragging Mouse
   useEffect(() => {
+    const element = document.querySelector("section");
+    // let pos = { startX: 0, startY: 0, scrollLeft: 0, scrollTop: 0 };
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
     const onMouseMove = (e) => {
-      console.info("mousemove", e);
+      e.preventDefault();
+      // const dx = e.pageX - element.offsetLeft;
+      // const dy = e.clientY - element.offsetTop;
+      // console.log(dx, dy);
+      // element.scrollLeft = pos.scrollLeft - dx;
+      // element.scrollTop = pos.scrollTop - dy;
+      // console.log(element.scrollLeft, element.scrollTop);
+      const dx = e.clientX - pos.x;
+      const dy = e.clientY - pos.y;
+      element.scrollTop = pos.top - dy;
+      element.scrollLeft = pos.left - dx;
     };
-    const onMouseUp = (e) => {
+
+    const onMouseUp = () => {
       window.removeEventListener("mousemove", onMouseMove);
       window.removeEventListener("mouseup", onMouseUp);
     };
+
     const onMouseDown = (e) => {
+      // pos = {
+      //   startX: e.pageX - element.offsetLeft,
+      //   startY: e.pageY - element.offsetTop,
+      //   scrollLeft: element.scrollLeft,
+      //   scrollTop: element.scrollTop,
+      //   // x: pos.x,
+      //   // y: pos.y,
+      // };
+      pos = {
+        left: element.scrollLeft,
+        top: element.scrollTop,
+        x: e.clientX,
+        y: e.clientY,
+      };
       window.addEventListener("mousemove", onMouseMove);
       window.addEventListener("mouseup", onMouseUp);
     };
@@ -38,14 +68,18 @@ function Game() {
       setImgSrc(images.HEDGE_MAZE.src);
   }, []);
 
-  return imgSrc ? (
-    <img
-      src={imgSrc}
-      alt="A Where's Waldo Drawing"
-      onDragStart={(e) => e.preventDefault()}
-    />
-  ) : (
-    <div>loading image...</div>
+  return (
+    <section>
+      {imgSrc ? (
+        <img
+          src={imgSrc}
+          alt="A Where's Waldo Drawing"
+          onDragStart={(e) => e.preventDefault()}
+        />
+      ) : (
+        <div>loading image...</div>
+      )}
+    </section>
   );
 }
 
