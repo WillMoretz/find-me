5a6b4de6a91c7f1af2d26994f19a67604a4723fe
Will Moretz <whmoretz@gmail.com>
2023-06-26 11:59:23 -0400

    added top bar elements

--- src/Components/GameOver.js
+++ src/Components/GameOver.js
@@ -1,7 +1,16 @@
 import React from "react";
+import waldoHead from "../img/waldo-head.jpg"
 
 function GameOver() {
-    return <div>Game Over</div>
+    return <div className="game-over-container">
+        <div className="top-bar">
+            <img src={waldoHead} aria-hidden />
+            <h2>Found Me!</h2>
+            <button type="button" onClick={() => passUpClick()}>
+            ✕
+            </button>
+        </div>
+    </div>
 }
 
 export default GameOver
