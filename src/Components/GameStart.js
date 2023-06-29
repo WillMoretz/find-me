f1f6a97772e84c0c298a028618bf9d7791aa375b
Will Moretz <whmoretz@gmail.com>
2023-06-29 17:04:39 -0400

    added elements to display props

--- src/Components/GameStart.js
+++ src/Components/GameStart.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-function GameStart() {
-  return <div>game start</div>;
+function GameStart(props) {
+  const { imgName, buttonEvent } = props;
+
+  return (
+    <div className="game-start-container">
+      <h2>{imgName}</h2>
+      <button type="button" onClick={() => buttonEvent()}>
+        Start Game
+      </button>
+    </div>
+  );
 }
 
 export default GameStart;
