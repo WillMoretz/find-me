41b49f1601432ab31a86d22718db36d996db7099
Will Moretz <whmoretz@gmail.com>
2023-07-07 12:32:43 -0400

    added elements

--- src/Components/GameStart.js
+++ src/Components/GameStart.js
@@ -1,15 +1,32 @@
 import React from "react";
+import Overlay from "./Overlay";
+import waldoHead from "../img/waldo-head.jpg";
+import "../styles/game-start.scoped.css";
 
 function GameStart(props) {
   const { imgName, buttonEvent } = props;
 
   return (
-    <div className="game-start-container">
-      <h2>{imgName}</h2>
-      <button type="button" onClick={() => buttonEvent()}>
-        Start Game
-      </button>
-    </div>
+    <>
+      <div className="game-start-container">
+        <div className="top-bar">
+          <img src={waldoHead} aria-hidden alt="waldo's head" />
+          <h2>{imgName}</h2>
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+          amet minima, ipsa deleniti ipsam obcaecati aliquid provident, dolore
+          id possimus nam vero molestias iure? Facere deserunt debitis quos hic
+          unde porro aliquid delectus repellendus adipisci aut ratione quae,
+          quam tenetur accusamus? Non distinctio omnis nesciunt placeat veniam!
+          Debitis, veniam veritatis.
+        </p>
+        <button type="button" onClick={() => buttonEvent()}>
+          Start Game
+        </button>
+      </div>
+      <Overlay />
+    </>
   );
 }
 
